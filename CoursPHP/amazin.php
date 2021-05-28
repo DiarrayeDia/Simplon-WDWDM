f210f78fe7cbb03a95ead63bc3b451bb91d480b4
DiarrayeDia <diarraye.dia@gmail.com>
2021-05-28 08:22:48 +0200

    step back warning: non-numeric value on line 85 ??

--- CoursPHP/amazin.php
+++ CoursPHP/amazin.php
@@ -16,6 +16,24 @@
 </head>
 
 <body>
+    <?php
+    function rating()
+    {
+        $rate = '';
+        for ($points = 0; $points < $_GET['result']; $points++) {
+            $rate .= '⭐️';
+        }
+
+        if ($_GET['result'] === "-1") {
+            $_GET['result'] === "0";
+        }
+        while ($points < 5) {
+            $rate .= '⚫️';
+        }
+        return $rate;
+    }
+
+    ?>
 
     <div class="container">
 
@@ -24,71 +42,80 @@
         <h2>Questionnaire de satisfaction</h2>
 
 
-<?php    
-    // step 0 : A afficher uniquement au chargement de la page //
-    if ($_GET == null) 
-    {
-        echo '<p>Vous avez contacté notre service client et nous aimerions avoir votre avis sur la qualité de ce service</p>
-            <p>Commencez le questionnaire : <a href="?step=1" role="button" class="btn btn-success">Commencer</a></p>';
-    }
+        <?php
+        // step 0 : A afficher uniquement au chargement de la page 
+        if ($_GET == null) {
+        ?>
+            <p>Vous avez contacté notre service client et nous aimerions avoir votre avis sur la qualité de ce service</p>
+            <p>Commencez le questionnaire : <a href="?step=1" role="button" class="btn btn-success">Commencer</a></p>
 
-    // Etape 1 : A afficher uniquement une fois que le boutton "Commencer" a été pressé //
-    if (isset($_GET['step']) && $_GET['step'] == '1') {
-        echo '
-        <h2>Question 1</h2>
-            <p>L\'agent a-t-il été agréable ?</p>
+        <?php
+            // Etape 1 : A afficher uniquement une fois que le boutton "Commencer" a été pressé
+        } elseif ($_GET['step'] === '1') {
 
-            <a href="?step=2" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
-            <a href="?step=2" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
-            <a href="?step=2" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->';
-    }
-     // Etape 2 : A afficher uniquement une fois que l'étape 1 a été résolue //
-    if (isset($_GET['step']) && $_GET['step'] == '2') {
-        echo '
-        <h2>Question 2</h2>
-            <p>L\'agent a-t-il compris votre problème ?</p>
-            <a href="?step=3" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
-            <a href="?step=3" role="button" class="btn btn-danger">non</a> <!-- rapporte 1 point -->
-            <a href="?step=3" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 0 point -->'
-        ;
-    }
-    // Etape 3 : A afficher uniquement une fois que l'étape 2 a été résolue //
-    if (isset($_GET['step']) && $_GET['step'] == '3') {
-        echo '
-        <h2>Question 3</h2>
-            <p>L\'agent a-t-il résolu votre problème ?</p>
-            <a href="?step=4" role="button" class="btn btn-success">oui</a> <!-- rapporte 1 point -->
-            <a href="?step=4" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->'
-        ;
-    }
+        ?>
+            <h2>Question 1</h2>
+            <p>L'agent a-t-il été agréable ?</p>
 
-    // Etape 4 : A afficher uniquement si "non" a été répondu à l'étape 3 //
-    if (isset($_GET['step']) && $_GET['step'] == '4') {
-        echo '
-            <p>Votre problème n\'a pas été résolu.</p>
-            <p>Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez :</p>';
-    
- /*  Coder ici un clavier numérique permettant de saisir le numéro de téléphone 
- Afficher ici le numéro de téléphone qui s'affiche au fur et à mesure de la saisie */  
 
-        echo '<p>Votre numéro : #numéro de téléphone saisi#</p>
-            <a href="?restart=y" role="button" class="btn btn-success">Valider</a>'
-        ;
+            <a href="?step=2&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
+            <a href="?step=2&result=<?= $_GET['result'] ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
+            <a href="?step=2&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
 
-    }
+        <?php
+            // Etape 2 : A afficher uniquement une fois que l'étape 1 a été résolue
+        } elseif ($_GET['step'] === '2') {
 
-    // Etape finale : A afficher si "oui" a été répondu à la question 3 ou si l'étape 4 a été résolue //
-    if (isset($_GET['restart']) && $_GET['restart'] == "y") {
-        echo 
-            '<p class="mt-5">Merci pour votre notation :⭐⭐⭐⚫⚫ </p> <!-- le nombre d\'étoiles représente le nombre de points cumulés -->';
-            
-    // Si un téléphone à été saisi, afficher "Vous serez rappelé très prochainement au #numéro de téléphone#"//    
-            echo '
+        ?>
+            <h2>Question 2</h2>
+            <p>L'agent a-t-il compris votre problème ?</p>
+            <a href="?step=3&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
+            <a href="?step=3&result=<?= $_GET['result'] ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
+            <a href="?step=3&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
+
+
+        <?php
+            // Etape 3 : A afficher uniquement une fois que l'étape 2 a été résolue 
+        } elseif ($_GET['step'] === '3') {
+
+        ?>
+            <h2>Question 3</h2>
+            <p>L'agent a-t-il résolu votre problème ?</p>
+            <a href="?step=4&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 1 point -->
+            <a href="?step=4n&result=<?= $_GET['result'] - 1 ?>&choice=no&phone" role="button" class="btn btn-danger">non</a> <!-- rapporte -1 point -->
+
+        <?php
+            // Etape 4 : A afficher uniquement si "non" a été répondu à l'étape 3 
+        } elseif ($_GET['step'] === '4') {
+
+        ?>
+            <p>Votre problème n'a pas été résolu.</p>
+
+            <!--Afficher ici le numéro de téléphone qui s'affiche au fur et à mesure de la saisie -->
+
+            <label for="phone">Pour être rappelé, entrez votre numéro de téléphone dans le clavier virtuel et validez : <br> </label>
+
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required>
+
+            <medium> Format: 0124567890 <br></medium>
+            <a href="?" role="button" class="btn btn-success">Valider</a>
+
+        <?php
+            // Etape finale : A afficher si "oui" a été répondu à la question 3 ou si l'étape 4 a été résolue 
+        } elseif ($_GET['step'] === '4') {
+
+        ?>
+            <p class="mt-5">Merci pour votre notation :
+                <?= rating(); ?>
+            </p> <!-- le nombre d\'étoiles représente le nombre de points cumulés -->
+
+            <!--Si un téléphone à été saisi, afficher "Vous serez rappelé très prochainement au #numéro de téléphone#"  -->
             <p class="mt-5">
-            <a href="?" role="button" class="btn btn-danger">Recommencer</a></p>';
-    }
+                <a href="?" role="button" class="btn btn-danger">Recommencer</a></p>
 
-    ?>
+        <?php
+        }
+        ?>
 
     </div>
 </body>
