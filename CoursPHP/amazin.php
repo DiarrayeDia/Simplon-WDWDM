e9fb3518913cee3601416606b152da0e252ff8ed
DiarrayeDia <diarraye.dia@gmail.com>
2021-05-31 20:21:16 +0200

    fix  result reference

--- CoursPHP/amazin.php
+++ CoursPHP/amazin.php
@@ -59,9 +59,9 @@
             <p>L'agent a-t-il été agréable ?</p>
 
 
-            <a href="?step=2&result=<?= $_GET['result'] + 2 ?>" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
-            <a href="?step=2&result=<?= $_GET['result'] + 0 ?>" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
-            <a href="?step=2&result=<?= $_GET['result'] + 1 ?>" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
+            <a href="?step=2&result= 2" role="button" class="btn btn-success">oui</a> <!-- rapporte 2 point -->
+            <a href="?step=2&result= 0" role="button" class="btn btn-danger">non</a> <!-- rapporte 0 point -->
+            <a href="?step=2&result= 1" role="button" class="btn btn-secondary">sans avis</a> <!-- rapporte 1 point -->
 
         <?php
             // Etape 2 : A afficher uniquement une fois que l'étape 1 a été résolue
